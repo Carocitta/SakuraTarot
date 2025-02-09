b3913c010ca524ca0bc77baa84f98cb28878052c
catmaluci <catmaluci@hotmail.com>
2025-02-09 16:07:07 +0100

    links added to header functional

---
+++ src/pages/PickACard.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import PickCard from "../components/PickCard/PickCard";
+
+function PickACard() {
+  return (
+    <>
+      <Header />
+      <PickCard />
+    </>
+  );
+}
+
+export default PickACard;
