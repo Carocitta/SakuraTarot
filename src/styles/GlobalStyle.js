a4f4d6c169646b4e42b8218bd06b270236285a9b
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-02-09 19:10:51 +0100

    Add App component and styles

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+*,
+::before,
+::after {
+  box-sizing: border-box;
+ }
+
+ :root {
+  font-family: Inter, system-ui, Avenir, Helvetica, Arial, sans-serif;
+
+}
+
+ body,
+ h1, h2, h3, h4, p {
+  margin: 0;
+  background-color: #FCCB4A;
+ }
+
+ html {
+   font-family: Verdana, Geneva, Tahoma, sans-serif;
+ }
+
+ ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+ }
+
+ img {
+ max-width: 100%
+ }
+
+ a {
+  text-decoration: none;
+  color: inherit;
+ }     
+
+ input,
+ textarea,
+ select {
+  font: inherit;
+  border: none;
+ }
+
+ button {
+  border: none;
+  background-color: transparent;
+  font: inherit;
+  cursor: grab;
+ }
+
+`;
+
+export default GlobalStyle;
