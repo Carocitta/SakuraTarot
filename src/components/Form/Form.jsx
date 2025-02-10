6c468cfb7e7fa4168edd200a933d7c5aa252e5bf
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-02-10 17:31:19 +0100

    added custom spanish alerts to validation

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,23 +1,37 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
 import { FormContainer, Input, SubmitButton } from "./Form.elements";
 
 const Form = () => {
+    const [name, setName] = useState("");
+    const navigate = useNavigate();
+    
     const handleSubmit = (e) => {
-      e.preventDefault();
-      console.log("Formulario enviado");
+        e.preventDefault();
+        navigate(/*/pick-a-card*/);
     };
   
     return (
         <>
             <FormContainer>
                 <form onSubmit={handleSubmit}>
-                    <Input type="text" maxLength={10} pattern="[A-Za-z]"
-                        title="Por favor, ingresa tu nombre"
-                        onChange={(e) => {
-                            const valor = e.target.value;
-                            if (!/^[A-Za-z]+$/.test(valor)) {
-                                console.log("Solo se permiten letras");
-                            }
-                        }} id="name" name="name" required />
+                <Input 
+                    type="text" 
+                    maxLength={10} 
+                    pattern="[A-Za-z]+" 
+                    title="Por favor, ingresa solo letras (sin números ni espacios)."
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    required 
+                    onInvalid={(e) => {
+                        if (!e.target.value) {
+                            e.target.setCustomValidity("Este campo es obligatorio.");
+                        } else {
+                            e.target.setCustomValidity("Por favor, ingresa solo letras (sin números ni espacios).");
+                        }
+                    }}
+                    onInput={(e) => e.target.setCustomValidity("")}
+                />
                     <SubmitButton type="submit" >
                     <p className="butText">Descubre tu fortuna</p>
                     </SubmitButton>
