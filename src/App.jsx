5a484cde296337865ac3d68f6b69f0ba6deb81b3
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-02-10 14:25:38 +0100

    Fix App

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import Header from "./components/Header/Header";
-import "./App.css";
 import Api from "./api/Api";
+import AppPageStyled from "./AppStyled";
+import image from "../src/assets/images/kero_logo.png";
 
 function App() {
   return (
-    <>
+    <AppPageStyled>
       <Header />
-      <p> Pagina de INICIO</p>
-      <h1>Datos traidos de API</h1>
+      <h1 className="main-title">SAKURA TAROT</h1>
+      <img className="kero-logo" src={image}></img>
+      <p>El poder de las cartas Clow te llama!</p>
+      <p> Introduce tu nombre y desata tu magia!</p>
       <Api />
-    </>
+    </AppPageStyled>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import App from "./components/App/App.jsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 import GlobalStyle from "./styles/GlobalStyle.js";
 import Router from "./application/Router.jsx";
-
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GlobalStyle />
     <Router />
-    <App />
   </StrictMode>
 );
