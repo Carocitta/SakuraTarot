bbd11fb8e3ab4c4df5bb71395c801c5f70c0f209
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-02-10 11:59:22 +0100

    Add icon button component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,8 @@
 import AppPageStyled from "./AppStyled";
 import image from "../../assets/images/kero_logo.png";
+import IconButton from "../IconButton/IconButton";
+import deleteIcon from "../../assets/images/delete-icon.png";
+import modifyIcon from "../../assets/images/modify-icon.png";
 
 function App() {
   return (
@@ -8,6 +11,8 @@ function App() {
       <img className="kero-logo" src={image}></img>
       <p>El poder de las cartas Clow te llama!</p>
       <p> Introduce tu nombre y desata tu magia!</p>
+      <IconButton icon={deleteIcon} />
+      <IconButton icon={modifyIcon} />
     </AppPageStyled>
   );
 }
---
+++ src/components/IconButton/IconButtonStyled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const IconButtonStyled = styled.button`
+  background-color: #9083e3;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+
+  &:active {
+    background-color: #795743;
+  }
+
+  .icon-image {
+    //pointer-events: none;
+  }
+`;
+
+export default IconButtonStyled;
