dca117c5d836049c843cc151b6c5c3858b7dca60
Carolina Mas <carolinamas@MacBook-Air-de-Carolina.local>
2025-02-17 10:01:40 +0100

    historial editado

--- src/components/HistorialComponents/HistorialList.jsx
+++ src/components/HistorialComponents/HistorialList.jsx
@@ -4,14 +4,14 @@ import modify from "../../assets/images/modify-icon.png";
 const HistorialList = ({ items }) => (
   <ul>
     {items.map((item, index) => (
-      <li key={index}>
-        {item.deleteIcon && (
-          <img src={item.deleteIcon} alt="Delete Icon" width="30" />
-        )}{" "}
-        {item.modifyIcon && (
-          <img src={item.modifyIcon} alt="Modify Icon" width="30" />
-        )}{" "}
-      </li>
+      // <li key={index}>
+      //   {item.deleteIcon && (
+      //     <img src={item.deleteIcon} alt="Delete Icon" width="30" />
+      //   )}{" "}
+      //   {item.modifyIcon && (
+      //     <img src={item.modifyIcon} alt="Modify Icon" width="30" />
+      //   )}{" "}
+      // </li>
     ))}
   </ul>
 );
