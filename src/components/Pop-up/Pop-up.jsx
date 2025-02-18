e26b2bd8774518f25b1e67f139ab8082758f47b8
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-02-18 13:31:07 +0100

    Update Pop-up.jsx

--- src/components/Pop-up/Pop-up.jsx
+++ src/components/Pop-up/Pop-up.jsx
@@ -4,9 +4,7 @@ import { PopupContainer, Overlay } from './Pop-up.styled';
 const Popup = ({ isOpen, onClose, children }) => {
   return (
     <>
-      {/* Esta es la capa que cubre toda la pantalla */}
       <Overlay onClick={onClose}>
-        {/* Este es el contenido del popup */}
         <PopupContainer onClick={(e) => e.stopPropagation()}>
           {children}
         </PopupContainer>                            
